07faa46083d2369d0e33cfd22b5c2f93e82755e8
Shams-Ur-Rehman <shamsur.rehman@xgrid.co>
2023-01-17 05:54:24 +0500

    Added a wrapper function and removed sleep commands from the script.

--- src/installer/px-installer.sh
+++ src/installer/px-installer.sh
@@ -4,6 +4,17 @@ source /src/config/px-config.sh
 source /src/config/config.sh
 source /src/commons/common-functions.sh
 
+# Wrapper function to check for a deployment in a namespace. If this is not use, we have to use either timeout or sleep commands
+wrapper_function() {
+  while true; do
+    dc=$(kubectl get deployments -n "$1" -o jsonpath='{.items[*].metadata.name}' | cut -d'%' -f1 | wc -w)
+    if [ "$dc" -gt 0 ]; then
+      break
+    fi
+    kubectl wait deployment "$2" --for=condition=Available -n "$1" --timeout=1h 2> /dev/null
+  done
+}
+
 # Installing pixie on a containerized environment using Helm.
 px_installer() {
 
@@ -11,13 +22,13 @@ px_installer() {
   helm repo add pixie-operator https://pixie-operator-charts.storage.googleapis.com 1>/dev/null
   helm repo update 1>/dev/null
   helm install pixie pixie-operator/pixie-operator-chart --set deployKey="$PX_DEPLOY_KEY" --set clusterName=minikube --namespace pl --create-namespace >/dev/null
-  log "${BROWN}[INFO]" "[INSTALLER]" "Waiting for Pixie deplyments to become available ${CC}"
-  kubectl wait deployment catalog-operator -n olm --for=condition=Available --timeout=1h 1>/dev/null
-  kubectl wait deployment olm-operator -n olm --for=condition=Available --timeout=1h 1>/dev/null
-  sleep 60
-  kubectl wait deployment vizier-operator -n px-operator --for=condition=Available --timeout=1h 1>/dev/null
-  sleep 40
-  kubectl wait deployment kelvin -n pl --for=condition=Available --timeout=1h 1>/dev/null
+  log "${BROWN}[INFO]" "[INSTALLER]" "Waiting for Pixie deployments to become available ${CC}"
+
+  wrapper_function "olm" "catalog-operator"
+  wrapper_function "olm" "olm-operator"
+  wrapper_function "px-operator" "vizier-operator"
+  wrapper_function "pl" "kelvin"
+
   log "${GREEN}[INFO]" "[INSTALLER]" "Pixie has been deployed to your cluster${CC}"
   exit 0
 }
--- src/rollback/px-rollback.sh
+++ src/rollback/px-rollback.sh
@@ -3,7 +3,6 @@ source /src/commons/common-functions.sh
 source /src/config/config.sh
 #waits for 5 minutes and then call rollback functions.
 pixie_rollback(){
-  sleep 300
   helm uninstall pixie -n pl
   kubectl delete namespace pl
   log "${GREEN}[INFO]" "[ROLLBACK]" "Pixie has been deleted from your cluster${CC}"
