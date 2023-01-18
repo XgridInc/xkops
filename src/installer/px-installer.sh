6f7f57433b4efd3ae420d3c0c1cb6793d0dd50f9
Shams-Ur-Rehman <shamsur.rehman@xgrid.co>
2023-01-18 05:04:43 +0500

    Restructured the code. It checks for deployments, pods and their status.

--- src/installer/px-installer.sh
+++ src/installer/px-installer.sh
@@ -4,32 +4,43 @@ source /src/config/px-config.sh
 source /src/config/config.sh
 source /src/commons/common-functions.sh
 
-# Wrapper function to check for a deployment in a namespace. If this is not use, we have to use either timeout or sleep commands
-wrapper_function() {
+# The wrapper_function ensures that the deployment and pods in a namespace are fully operational by waiting for them to be in a running state.
+wait_for_deploy/pod() {
   while true; do
     dc=$(kubectl get deployments -n "$1" -o jsonpath='{.items[*].metadata.name}' | cut -d'%' -f1 | wc -w)
     if [ "$dc" -gt 0 ]; then
+      pods=$(kubectl get pods -n "$1" -o jsonpath='{.items[*].metadata.name}' | cut -d'%' -f1)
+      for pod in $pods; do
+        while true; do
+          status=$(kubectl get pods $pod -n "$1" -o jsonpath='{.status.phase}' | cut -d'%' -f1)
+          if [[ $status == "Succeeded" || $status == "Running" ]]; then
+            echo "Pod $pod reached the desired status: $status" 1> /dev/null
+            break
+          else
+            echo "Waiting for pod $pod to reach the desired status: $status" 1> /dev/null
+          fi
+        done
+      done
       break
+    else
+      echo "Number of deployment is Zero. Waiting for deployments to populate." 1> /dev/null
     fi
-    kubectl wait deployment "$2" --for=condition=Available -n "$1" --timeout=1h 2> /dev/null
-  done
+done
 }
 
 # Installing pixie on a containerized environment using Helm.
 px_installer() {
-
   log "${YELLOW}[INFO]" "[INSTALLER]" "Deploying Pixie using Helm..${CC}"
   helm repo add pixie-operator https://pixie-operator-charts.storage.googleapis.com 1>/dev/null
   helm repo update 1>/dev/null
   helm install pixie pixie-operator/pixie-operator-chart --set deployKey="$PX_DEPLOY_KEY" --set clusterName=minikube --namespace pl --create-namespace >/dev/null
   log "${BROWN}[INFO]" "[INSTALLER]" "Waiting for Pixie deployments to become available ${CC}"
-
-  wrapper_function "olm" "catalog-operator"
-  wrapper_function "olm" "olm-operator"
-  wrapper_function "px-operator" "vizier-operator"
-  wrapper_function "pl" "kelvin"
-
+  # Calling wait_for_deploy/pod function with namespace as an argument
+  wait_for_deploy/pod "$OLMNS"
+  wait_for_deploy/pod "$PXOPNS"
+  wait_for_deploy/pod "$PLNS"
   log "${GREEN}[INFO]" "[INSTALLER]" "Pixie has been deployed to your cluster${CC}"
   exit 0
 }
+
 px_installer
