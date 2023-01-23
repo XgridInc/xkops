d51e9b55ac15ad3f7a8183abc1e4bc4c0e09862c
Noman Ali Bajwa <nmnbajwa@gmail.com>
2023-01-23 18:19:47 +0500

    Formatting and metadata changes.

--- src/test/px-test.sh
+++ src/test/px-test.sh
@@ -8,18 +8,26 @@ print_prompt() {
 
 }
 
-# checks if the pods in all the pixie namespaces are runnning or not.
-pod_status_verifier "$PX_NAMESPACES"
-
-# check Vizier health on the cluster 
+# This function checks the status of the Pixie Vizier component. The healthy status is defined by status "CS_HEALTHY"
 check_vizier() {
+    
+    # authentication of pixie CLI
+    px auth login --api_key "$PX_API_KEY"
 
+    #TODO: [noman-xg] check specifically against the name of the cluster instead of tailing.
+    # get status of Vizier.
     vizier_status=$(px get viziers | tail -1 | awk '{print $8}')
+    
+    # check if Vizier is in healthy state or not.
     if [ "$vizier_status" != "CS_HEALTHY" ]; then
         log "${RED}[FAILED]" "[TEST]" "Pixie Vizier is not healthy state.${CC}${YELLOW} Vizier Status: $vizier_status ${CC}"
     else
-        log "${RED}[PASSED]" "[TEST]" "Pixie Vizier is in healthy state.${CC}${YELLOW} Vizier Status: $vizier_status ${CC}"
+        log "${GREEN}[PASSED]" "[TEST]" "Pixie Vizier is in healthy state.${CC}${YELLOW} Vizier Status: $vizier_status ${CC}"
     
     fi
-
 }
+
+print_prompt
+# An array of Pixie namespaces is passed to the function. 
+pod_status_verifier "${PX_NAMESPACES[@]}"
+check_vizier
