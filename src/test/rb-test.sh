9fb234aba58fff2daf342c9a1e70971da42e3465
ali.khan <ali.khan@xgrid.co>
2023-01-23 17:42:06 +0500

    updated rb-test

--- src/test/rb-test.sh
+++ src/test/rb-test.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 source /src/commons/common-functions.sh
 source /src/config/config.sh
+source /src/config/rb-config.sh
 
+print_prompt() {
+    log "${CYAN}[INFO]" "[TEST]" "Initiating test plan for Robusta."
 
+}
 
-namespaces=("robusta")
-pod_status_verifier "${namespaces[@]}"
+check_robusta_pod_status() {
+    # checks if the pods in all the pixie namespaces are runnning or not.
+    pod_status_verifier "$RB_NAMESPACES"
+}
+
