ecc9631f5626fdba395baed556e0ff0888a9787e
wahajXgrid <wahaj.ali@xgrid.co>
2023-01-23 16:36:04 +0500

    pod verifier added

--- src/commons/common-functions.sh
+++ src/commons/common-functions.sh
@@ -57,3 +57,25 @@ check_permissions() {
         exit 1
     fi
 }
+
+pod_verifier() {
+    namespaces=("$@")
+    for namespace in "${namespaces[@]}"; do
+        pods=$(kubectl get pods -n "$namespace" -o jsonpath='{.items[*].metadata.name}')
+
+        for pod in $pods; do
+            status=$(kubectl get pods "$pod" -n "$namespace" -o jsonpath='{.status.phase}')
+            while [ "$status" == "ContainerCreating" ]; do
+                echo "Waiting for pod $pod in namespace $namespace to become Running or Completed..."
+                kubectl wait --for=condition=Ready pod/"$pod" -n "$namespace"
+                status=$(kubectl get pods "$pod" -n "$namespace" -o jsonpath='{.status.phase}')
+            done
+            if [ "$status" != "Running" ] && [ "$status" != "Completed" ]; then
+                echo "Error: Pod $pod in namespace $namespace is not in a Running or Completed state"
+                exit 1
+            fi
+        done
+    done
+    echo "All pods in the namespaces are in Running or Completed state"
+    exit 0
+}
