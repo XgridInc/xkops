f7c486bee6d005704649c33afcf82d0a04899912
abid_iqbal <abid.iqbal@xgrid.co>
2023-01-24 12:31:33 +0500

    Updated kc-test file and added a wrapper around install command of kubecost to avoid unnecessary errors

--- src/test/kc-test.sh
+++ src/test/kc-test.sh
@@ -8,27 +8,42 @@ print_prompt() {
     log "${CYAN}[INFO]" "[TEST]" "Initiating test plan for Kubecost.${CC}"
 
 }
-# pod_status_verifier kubecost
 
-check_kubectl_cost() {
+# This Function checks whether kubectl_cost plugin is installed or not
+# This plugin is used to get cost data from the cluster.
+check_kubectl_cost_plugin() {
 
-    # Check if kubectl_cost is installed
-    if command -v kubectl cost &>/dev/null; then
-        echo "kubectl cost is already installed. Using it to get cost"
+    # Check if kubectl_cost plugin is installed
+    if command -v kubectl-cost &>/dev/null; then
+        log "${GREEN}[PASSED]" "[TEST]" "kubectl cost plugin is already installed.${CC}"
     else
-        echo "Installing kubectl cost now"
+        log "${RED}[FAILED]" "[TEST]" "Installing kubectl cost plugin now.${CC}"
         install_kubectl_cost
     fi
-
-    kubectl cost namespace --show-all-resources
+    # Checks if kubectl_cost plugin can retrieve cost data using kubecost-cost-analyzer service.
+    get_cost
 
 }
+
+# This Function uses kubecost-cost-analyzer service to get cost data from the cluster.
+# If the data is retrieved successfully, it means kubecost is installed in the cluster and can access cluster resources.
+get_cost() {
+    if kubectl cost namespace --show-all-resources &>/dev/null; then
+        log "${GREEN}[PASSED]" "[TEST]" "Kubecost is installed successfully.${CC}"
+        exit 0
+    else
+        log "${RED}[FAILED]" "[TEST]" "Kubecost is not installed in the cluster.${CC}"
+        exit 1
+    fi
+}
+
+# Function to install kubectl_cost plugin in the cluster
 install_kubectl_cost() {
     os=$(uname | tr '[:upper:]' '[:lower:]') &&
         arch=$(uname -m | tr '[:upper:]' '[:lower:]' | sed -e s/x86_64/amd64/) &&
         curl -s -L https://github.com/kubecost/kubectl-cost/releases/latest/download/kubectl-cost-$os-$arch.tar.gz | tar xz -C /tmp &&
         chmod +x /tmp/kubectl-cost &&
-        cp /tmp/kubectl-cost /usr/local/bin/kubectl-cost
+        mv /tmp/kubectl-cost /usr/local/bin/kubectl-cost
 }
 print_prompt
 # Kubecost namespace is passed to the function.
