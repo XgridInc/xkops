600a4a2c4caa1d196bc133a6835d34851f6b8730
yousaf-aftab <yousaf.aftab@xgrid.co>
2023-01-25 00:52:18 +0500

    added check on vizier status in installer script

--- src/installer/px-installer.sh
+++ src/installer/px-installer.sh
@@ -41,10 +41,18 @@ px_installer() {
   helm repo update 1>/dev/null
   helm install pixie pixie-operator/pixie-operator-chart --set deployKey="$PX_DEPLOY_KEY" --set clusterName=xgrid-website-migration --namespace pl --create-namespace >/dev/null
   log "${BROWN}[INFO]" "[INSTALLER]" "Waiting for Pixie deployments to become available ${CC}"
+  
   # Calling wait_for_deploy/pod function with namespace as an argument
   wait_for_deployment "$OLMNS"
   wait_for_deployment "$PXOPNS"
   wait_for_deployment "$PLNS"
+  
+  #check for vizier status
+  vizier_status=0
+  while [ "$vizier_status" -ne 1 ]; do
+    vizier_status=$(px get viziers -o json | jq -c '. | select(.ClusterName == "xgrid-website-migration") | .Status')
+  done
+  
   log "${GREEN}[INFO]" "[INSTALLER]" "Pixie has been deployed to your cluster${CC}"
   exit 0
 }
