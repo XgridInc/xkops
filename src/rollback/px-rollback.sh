bf6a239b5e8b0a2b57d213a45ec9d9435b78b2ce
ali.khan <ali.khan@xgrid.co>
2023-01-25 18:50:12 +0500

    suppressed stdout and stderr in px-rollback

--- src/rollback/px-rollback.sh
+++ src/rollback/px-rollback.sh
@@ -3,8 +3,8 @@ source /src/commons/common-functions.sh
 source /src/config/config.sh
 #waits for 5 minutes and then call rollback functions.
 pixie_rollback(){
-  helm uninstall pixie -n pl
-  kubectl delete namespace pl
+  helm uninstall pixie -n pl &>/dev/null
+  kubectl delete namespace pl &>/dev/null
   log "${GREEN}[INFO]" "[ROLLBACK]" "Pixie has been deleted from your cluster${CC}"
 }
 pixie_rollback
