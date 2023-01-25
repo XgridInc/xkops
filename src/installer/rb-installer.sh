2097c36201e282119b65296280f27f51455f8030
ali.khan <ali.khan@xgrid.co>
2023-01-25 17:30:07 +0500

    updated rb-installer with log watcher for runner

--- src/installer/rb-installer.sh
+++ src/installer/rb-installer.sh
@@ -18,9 +18,10 @@ rb_installer() {
     # Check if Helm is installed.
     if command -v helm &>/dev/null; then
         # If Helm is present, use it to install Robusta.
-        helm repo add robusta https://robusta-charts.storage.googleapis.com && helm repo update &> /dev/null
-        helm install robusta robusta/robusta -f "$PREFLIGHT_DIR_PATH/generated_values.yaml" -n robusta --create-namespace &> /dev/null
+        helm repo add robusta https://robusta-charts.storage.googleapis.com && helm repo update &>/dev/null
+        helm install robusta robusta/robusta -f "$PREFLIGHT_DIR_PATH/generated_values.yaml" -n robusta --create-namespace &>/dev/null
         kubectl -n robusta wait deployment robusta-runner robusta-forwarder --for=condition=Available --timeout=1h
+        watch_runner_logs
         log "${GREEN}[INFO]" "[INSTALLER]" "Robusta sucessfully installed.${CC}"
         exit 0
 
@@ -31,6 +32,22 @@ rb_installer() {
         exit 1
 
     fi
+}
+
+watch_runner_logs() {
+
+    #This functions watches logs of robusta-runner and checks if all the actions are loaded
+
+    kubectl -n robusta logs -f -l app=robusta-runner | while read line; do
+        echo "$line" | grep -E ".*Initializing jobs cache.*" >/dev/null
+        result=$?
+        if [ $result -eq 0 ]; then
+            echo "Initializing jobs cache found in logs"
+            pkill -f "kubectl -n robusta logs -f -l app=robusta-runner"
+            break
+        fi
+    done
+
 }
 
 # Run the script.
