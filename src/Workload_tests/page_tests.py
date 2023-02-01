83c72d7313ba1e769a5b24285c8920ae4b66cf14
yousaf-aftab <yousaf.aftab@xgrid.co>
2023-02-01 13:20:04 +0500

    wrapped the code in functions

--- src/Workload_tests/page_tests.py
+++ src/Workload_tests/page_tests.py
@@ -1,5 +1,6 @@
 """ XGRID WEBSITE PAGE TESTS """
 import config
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,48 +9,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 # Create an instance of the Chrome driver
 options = webdriver.ChromeOptions()
 options.add_argument("--start-fullscreen")
-
 driver = webdriver.Chrome(options=options)
 
-# Navigate to the website home page
-driver.get(config.HOME_PAGE)
-
-# Navigate to xgrid career's page
-driver.get(config.CAREER_PAGE)
-
-# Navigate to xgrid DevOps page using click
-driver.find_element(By.XPATH, config.DEVOPS_PAGE).click()
-WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, config.DEVOPS_XPATH))
-)
+def xgrid_page(page_url):
+    """ functions to call the URLs of the webpages of website. """
+    try:
+        driver.get(page_url)
+    except selenium.common.exceptions.WebDriverException as e:
+        print(f"Failed to load the homepage: {e}")
+    except Exception as e:
+        print("An unknown error occured:", e)
 
 
-# Navigate to xgrid web and mobile page
-driver.find_element(By.XPATH, config.WAM_XPATH).click()
+def xgrid_page_test():
+    """ functions to tests the webpages of website. """
+    
+    for url in config.XGRID_URLS:
+        xgrid_page(url)
 
-# Navigate to xgrid company's page
-driver.get(config.COMPANY_PATH)
 
-# navigate to Xgrid resources page.
-driver.get(config.RESOURCES_PATH)
-
-# Navigate to xgrid blogs and then opens up TLS blog
-driver.get(config.BLOG_PATH)
-
-# Navigate to xgrid information and security page
-driver.get(config.ISP_PATH)
-
-# Navigate to xgrid open positions page
-driver.get(config.OPEN_POSITIONS_PATH)
-
-# Navigate to xgrid privacy policy page
-driver.get(config.PRIVACY_POLICY_PATH)
-
-# Navigate to xgrid terms and conditions page
-driver.get(config.TAC_PATH)
-
-# Navigate to xgrid thankyou page
-driver.get(config.THANKYOU_PATH)
-
+#xgrid pages test function call
+xgrid_page_test()
 # Close the browser
 driver.quit()
