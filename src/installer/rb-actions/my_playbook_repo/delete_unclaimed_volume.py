0952ff09a200d5e22bae7de1ac90727bba59852c
wahajXgrid <wahaj.ali@xgrid.co>
2023-02-10 12:59:36 +0500

    lint resolved

--- src/installer/rb-actions/my_playbook_repo/delete_unclaimed_volume.py
+++ src/installer/rb-actions/my_playbook_repo/delete_unclaimed_volume.py
@@ -2,15 +2,8 @@
 Module for deleting a persistent volume
 """
 
-from robusta.api import (
-    Finding,
-    FindingSource,
-    FindingType,
-    MarkdownBlock,
-    PersistentVolumeEvent,
-    action,
-    logging,
-)
+from robusta.api import (Finding, FindingSource, FindingType, MarkdownBlock,
+                         PersistentVolumeEvent, action, logging)
 
 
 @action
