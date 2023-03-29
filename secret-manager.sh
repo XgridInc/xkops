6c88086f05962db023e739463c5d6b78fec70adf
sundasfatema <sundasfatima1996@gmail.com>
2023-03-29 15:06:16 +0500

    Secret Manager Setup v2.0

--- secret-manager.sh
+++ secret-manager.sh
@@ -14,31 +14,22 @@ helm install -n kube-system secrets-provider-aws aws-secrets-manager/secrets-sto
 
 # Prompt the user to enter the values for the variables
 read -p "Enter the cluster region: " region
-read -p "Enter the name of the secret: " secret_name
-read -p "Enter the AWS access key ID: " access_key_id
-read -p "Enter the AWS secret access key: " secret_access_key
-read -p "Enter the AWS session token: " session_token
-read -p "Enter the Robusta UI API key: " robusta_ui_api_key
-read -p "Enter the Slack API key: " slack_api_key
-read -p "Enter the Pixie API key: " pixie_api_key
-read -p "Enter the Pixie deploy key: " pixie_deploy_key
 
 # Create the secret in the AWS Secrets Manager
-aws --region "$region" secretsmanager create-secret \
-    --name "$secret_name" \
-    --secret-string '{"AWS_ACCESS_KEY_ID":"'"$access_key_id"'", "AWS_SECRET_ACCESS_KEY":"'"$secret_access_key"'","AWS_SESSION_TOKEN":"'"$session_token"'","ROBUSTA_UI_API_KEY":"'"$robusta_ui_api_key"'","SLACK_API_KEY":"'"$slack_api_key"'","PX_API_KEY":"'"$pixie_api_key"'","PX_DEPLOY_KEY":"'"$pixie_deploy_key"'"}'
+aws --region "$region" secretsmanager  create-secret --name xkops-creds-secrets --secret-string '{"AWS_ACCESS_KEY_ID":"<aws-access-key-id>", "AWS_SECRET_ACCESS_KEY":"<aws-secret-access-key>","AWS_SESSION_TOKEN":"<aws-session-token>","ROBUSTA_UI_API_KEY":"<robusta-ui-api-key>","SLACK_API_KEY":"<slack-api-key>","PX_API_KEY":"<pixie-api-key>","PX_DEPLOY_KEY":"<pixie-deploy-key>"}'
 
 # Prompt the user to enter the values for the variables
-read -p "Enter the name of the IAM policy: " policy_name
 read -p "Enter the ARN of the secret: " secret_arn
 
-aws --region "$region" --query Policy.Arn --output text iam create-policy --policy-name "$policy_name" --policy-document '{     "Version": "2012-10-17",     "Statement": [ {         "Effect": "Allow",         "Action": ["secretsmanager:GetSecretValue", "secretsmanager:DescribeSecret", "sts:AssumeRoleWithWebIdentity"],         "Resource": ["'"$secret_arn"'"]     } ] }'
+aws --region "$region" --query Policy.Arn --output text iam create-policy --policy-name xkops-creds-secret-policy --policy-document '{     "Version": "2012-10-17",     "Statement": [ {         "Effect": "Allow",         "Action": ["secretsmanager:GetSecretValue", "secretsmanager:DescribeSecret", "sts:AssumeRoleWithWebIdentity"],         "Resource": ["'"$secret_arn"'"]     } ] }'
+
+# Prompt the user to enter the values for the variables
+read -p "Enter the Cluster Name: " cluster_name
 
 # Associate the IAM OIDC provider with the EKS cluster
 eksctl utils associate-iam-oidc-provider --region="$region" --cluster="$cluster_name" --approve
 
 # Prompt the user to enter the values for the variables
-read -p "Enter the name of the Service Account : " sa_name
 read -p "Enter the Policy ARN: " policy_arn
 
-eksctl create iamserviceaccount --name "$sa_name" --namespace xkops --region="$region" --cluster "$cluster_name" --attach-policy-arn "$policy_arn" --approve --override-existing-serviceaccounts
+eksctl create iamserviceaccount --name xkops-secret-sa --namespace xkops --region="$region" --cluster "$cluster_name" --attach-policy-arn "$policy_arn" --approve --override-existing-serviceaccounts
