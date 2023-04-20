9300300cbd9566e43270edb6518059c61b6308bd
Abid Iqbal <aiqbal@xgrid.co>
2023-04-20 12:57:09 +0500

    Added licensing in secret manager automation files

--- secret-manager/secret-manager.sh
+++ secret-manager/secret-manager.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright (c) 2023, Xgrid Inc, https://xgrid.co
+
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Add the Secrets Store CSI Driver Helm repository
 helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
 
